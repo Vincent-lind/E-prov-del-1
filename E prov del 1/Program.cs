aff4d17c33791ed3e1fc579c3dc3b6e51cbf4016
Vincent Lind <vincent.lind@elev.ga.ntig.se>
2024-12-05 13:26:52 +0100

    wefpojfwoejgv

--- E prov del 1/Program.cs
+++ E prov del 1/Program.cs
@@ -1,1 +1,111 @@
 ﻿
+Console.WriteLine("Skriv hur mycket hp du skal ha det ska vara mellan 1 till 20");
+
+string glurb = Console.ReadLine();    //om jag haft tid skulle jag skrivit kåden så gör en sån [ såna = för numrets värde och sedan en ]
+
+if (glurb == "1") {
+    Console.Write("[");
+Console.Write("=");
+Console.Write("]");
+}
+else if (glurb == "2") {
+    Console.WriteLine("[==]");
+    Console.WriteLine("[=]");
+}
+else if (glurb == "3") {
+     Console.WriteLine("[===]");
+ Console.WriteLine("[==]");
+    Console.WriteLine("[=]");
+}
+else if (glurb == "4") {
+    Console.WriteLine("[====]");
+ Console.WriteLine("[===]");
+ Console.WriteLine("[==]");
+    Console.WriteLine("[=]");
+}
+else if (glurb == "5") {
+    Console.Write("[");
+    Console.Write("=====");
+Console.Write("]");
+}
+else if (glurb == "6") {
+    Console.Write("[");
+Console.Write("======");
+Console.Write("]");
+}
+else if (glurb == "7") {
+    Console.Write("[");
+    Console.Write("=======");
+Console.Write("]");
+}
+else if (glurb == "8") {
+    Console.Write("[");
+    Console.Write("========");
+Console.Write("]");
+}
+else if (glurb == "9") {
+    Console.Write("[");
+    Console.Write("=========");
+Console.Write("]");
+}
+else if (glurb == "10") {
+    Console.Write("[");
+    Console.Write("==========");
+Console.Write("]");
+}
+else if (glurb == "11") {
+    Console.Write("[");
+Console.Write("===========");
+Console.Write("]");
+}
+else if (glurb == "12") {
+    Console.Write("[");
+    Console.Write("============");
+Console.Write("]");
+}
+else if (glurb == "13") {
+    Console.Write("[");
+    Console.Write("=============");
+Console.Write("]");
+}
+else if (glurb == "14") {
+    Console.Write("[");
+    Console.Write("==============");
+Console.Write("]");
+}
+else if (glurb == "15") {
+    Console.Write("[");
+    Console.Write("===============");
+Console.Write("]");
+}
+else if (glurb == "16") {
+    Console.Write("[");
+Console.Write("================");
+Console.Write("]");
+}
+else if (glurb == "17") {
+    Console.Write("[");
+    Console.Write("=================");
+Console.Write("]");
+}
+else if (glurb == "18") {
+    Console.Write("[");
+    Console.Write("==================");
+Console.Write("]");
+}
+else if (glurb == "19") {
+    Console.Write("[");
+    Console.Write("==================");
+Console.Write("]");
+}
+else if (glurb == "20") {
+    Console.Write("[");
+    Console.Write("====================");
+Console.Write("]");
+}
+else {
+    Console.WriteLine("bähöver ett numer som är från 1 till 20 din drånnt!");
+}
+ 
+
+Console.ReadLine();
